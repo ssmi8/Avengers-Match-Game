da999945a4efc563c9b2a2873ed64d0e5d9cc4e1
ssmi8 <smithsteve8@hotmail.com>
2022-04-21 16:56:12 +0000

    card images sorted in a data function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span');
+const playerLives  = 5;
 
-const grid = document/querySelector('.grid')
+//Link 
+playerLivesCount.textContent = playerLives;
 
-// grid to be used for the game
+// Generate card data
 
-function createBoard() {
-    for (let i = 0; i < cardArray.length; i++) {
-        var card = dcoument.createElement('img')
-        card.setAttribute('src', 'images/logo.jpeg')
-    }
-}
+const getData = () => [
+    { imgSrc: ".assets/images/black_panther.png", name: "black panther" },
+    { imgSrc: ".assets/images/captain_america.png", name: "captain america" },
+    { imgSrc: ".assets/images/captain_marvel.jpg", name: "captain marvel" },
+    { imgSrc: ".assets/images/hulk.png", name: "hulk" },
+    { imgSrc: ".assets/images/iron_man.jpg", name: "iron man" },
+    { imgSrc: ".assets/images/scarlet_witch.png", name: "scarlet witch" },
+    { imgSrc: ".assets/images/spider_man.jpg", name: "spider man" },
+    { imgSrc: ".assets/images/thor.png", name: "thor" },
+]
+
