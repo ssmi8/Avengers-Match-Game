e478350d62893e8c9f16fa20642972bcab04d924
ssmi8 <smithsteve8@hotmail.com>
2022-04-21 16:22:23 +0000

    Add favicon and link css stylesheet

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+const grid = document/querySelector('.grid')
+
+// grid to be used for the game
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = dcoument.createElement('img')
+        card.setAttribute('src', 'images/logo.jpeg')
+    }
+}
