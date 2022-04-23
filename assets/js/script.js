5955056187f384b95cbeb32ad24596ba83e389aa
ssmi8 <smithsteve8@hotmail.com>
2022-04-23 13:53:15 +0000

    Correct img location to produce image on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,22 @@ playerLivesCount.textContent = playerLives;
 // Generate card data for the game to work
 
 const getData = () => [
-    { imgSrc: ".assets/images/black_panther.png", name: "black panther" },
-    { imgSrc: ".assets/images/captain_america.png", name: "captain america" },
-    { imgSrc: ".assets/images/captain_marvel.jpg", name: "captain marvel" },
-    { imgSrc: ".assets/images/hulk.png", name: "hulk" },
-    { imgSrc: ".assets/images/iron_man.jpg", name: "iron man" },
-    { imgSrc: ".assets/images/scarlet_witch.png", name: "scarlet witch" },
-    { imgSrc: ".assets/images/spider_man.jpg", name: "spider man" },
-    { imgSrc: ".assets/images/thor.png", name: "thor" },
-    { imgSrc: ".assets/images/black_panther.png", name: "black panther" },
-    { imgSrc: ".assets/images/captain_america.png", name: "captain america" },
-    { imgSrc: ".assets/images/captain_marvel.jpg", name: "captain marvel" },
-    { imgSrc: ".assets/images/hulk.png", name: "hulk" },
-    { imgSrc: ".assets/images/iron_man.jpg", name: "iron man" },
-    { imgSrc: ".assets/images/scarlet_witch.png", name: "scarlet witch" },
-    { imgSrc: ".assets/images/spider_man.jpg", name: "spider man" },
-    { imgSrc: ".assets/images/thor.png", name: "thor" },
+    { imgSrc: "assets/images/black_panther.png", name: "black panther" },
+    { imgSrc: "assets/images/captain_america.png", name: "captain america" },
+    { imgSrc: "assets/images/captain_marvel.jpg", name: "captain marvel" },
+    { imgSrc: "assets/images/hulk.png", name: "hulk" },
+    { imgSrc: "assets/images/iron_man.jpg", name: "iron man" },
+    { imgSrc: "assets/images/scarlet_witch.png", name: "scarlet witch" },
+    { imgSrc: "assets/images/spider_man.jpg", name: "spider man" },
+    { imgSrc: "assets/images/thor.png", name: "thor" },
+    { imgSrc: "assets/images/black_panther.png", name: "black panther" },
+    { imgSrc: "assets/images/captain_america.png", name: "captain america" },
+    { imgSrc: "assets/images/captain_marvel.jpg", name: "captain marvel" },
+    { imgSrc: "assets/images/hulk.png", name: "hulk" },
+    { imgSrc: "assets/images/iron_man.jpg", name: "iron man" },
+    { imgSrc: "assets/images/scarlet_witch.png", name: "scarlet witch" },
+    { imgSrc: "assets/images/spider_man.jpg", name: "spider man" },
+    { imgSrc: "assets/images/thor.png", name: "thor" },
 ];
 
 
