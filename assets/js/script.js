f6ba7c5084d9fa07395c1f231e1ca7351514b704
ssmi8 <smithsteve8@hotmail.com>
2022-04-25 15:34:34 +0000

    feat: add toggle function to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,10 @@ const cardGenerator = () => {
     section.appendChild(card);
     card.appendChild(face);
     card.appendChild(back);
+
+    card.addEventListener('click', (e) => {
+        card.classList.toggle('commandCard');
+    })
     });
 };
    
